6522a3cbc4d0f060f029d9f55b7fcfe13afaa39b
the-utkarshjain <utkarsh.j.99@gmail.com>
2020-05-25 17:18:03 +0300

    updating about

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather Website - NodeJS
+
+This web application helps you to check the real time weather any where all over the world. I have developed this application on NodeJS and used data and API provided by [weatherstack.com](https://weatherstack.com/) and [mapbox.com](https://www.mapbox.com/).
+
+**Hosted on:** https://utkarsh-web-server.herokuapp.com/ 
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress celsius out. It feels like ' + body.current.feelslike + 'degree celsius.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress celsius out. It feels like ' + body.current.feelslike + ' degree celsius.')
         }
     })
 }
